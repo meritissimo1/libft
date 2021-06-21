37f00d348a7ca3b4693d6f46c3a196b8bdac4951
meritissimo1 <marcelomoreira1905@gmail.com>
2021-06-21 12:46:05 -0300

    finalizado

---
+++ ft_split.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/21 12:29:01 by user42            #+#    #+#             */
+/*   Updated: 2021/06/21 12:30:32 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static	int	ft_count_words(const char *s, char c)
+{
+	int	c_words;
+	int	trigger;
+
+	c_words = 0;
+	trigger = 0;
+	while (*s != '\0')
+	{
+		if (*s != c && trigger == 0)
+		{
+			trigger = 1;
+			c_words++;
+		}
+		else if (*s == c)
+			trigger = 0;
+		s++;
+	}
+	return (c_words);
+}
+
+static char	*ft_word_dup(const char *s, size_t start, size_t finish)
+{
+	char	*word;
+	int		i;
+	int		printable;
+
+	i = 0;
+	printable = 0;
+	word = malloc(((finish - start) + 1) * sizeof(char));
+	while (start < finish)
+	{
+		if (ft_isprint(s[start]))
+			printable++;
+		word[i] = s[start];
+		i++;
+		start++;
+	}
+	word[i] = '\0';
+	if (printable > 0)
+		return (word);
+	else
+		return (0);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**strs;
+	size_t	i;
+	size_t	j;
+	int		index;
+
+	strs = malloc((ft_count_words(s, c) + 1) * sizeof(char *));
+	if (!s || !strs)
+		return (0);
+	i = 0;
+	j = 0;
+	index = 0;
+	while (i <= ft_strlen(s))
+	{
+		if (s[i] != c && index < 0)
+			index = i;
+		else if ((s[i] == c || i == ft_strlen(s)) && index >= 0)
+		{
+			if ((size_t)index < i)
+				strs[j++] = ft_word_dup(s, index, i);
+			index = -1;
+		}
+		i++;
+	}
+	strs[j] = 0;
+	return (strs);
+}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marcrodr <marcrodr@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/09 22:25:36 by user42            #+#    #+#             */
-/*   Updated: 2021/06/19 10:35:01 by marcrodr         ###   ########.fr       */
+/*   Updated: 2021/06/20 18:29:28 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 		s1++;
 	i = ft_strlen(s1);
 	while (i && ft_strchr(set, s1[i]))
-	i = ft_strlen(s1); // pega o índice do final da string
-	while (i && ft_strchr(set, s1[i])) // percorre s1 do fim até não encontrar set.
+	i = ft_strlen(s1); 
+	while (i && ft_strchr(set, s1[i]))
 		i--;
 	return (ft_substr(s1, 0, i + 1));
 }
